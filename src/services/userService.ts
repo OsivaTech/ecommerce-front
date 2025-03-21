b188b9336469f76dc82891f426bceebf9e787a35
Adnilton <adniltonweb@gmail.com>
2025-03-21 16:27:38 -0300

    ajuste na chamada

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,7 @@
 import { httpService } from './httpService'
 
 export const userService = {
-  getProducts: () => httpService.get('/products'),
+  getProducts: () => httpService.get('/products/featured'),
   /*   getUserById: (id: number) => httpService.get(`/users/${id}`),
   createUser: (data: any) => httpService.post("/users", data),
   updateUser: (id: number, data: any) => httpService.put(`/users/${id}`, data),
