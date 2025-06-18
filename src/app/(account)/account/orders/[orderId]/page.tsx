609b4536d2180255cc20aaebd368ea8a7ef9bc07
Francisco Augusto dos Santos <santos.frau@gmail.com>
2025-06-17 22:00:51 -0300

    feat: aprimorar página de detalhes do pedido com carregamento assíncrono
    - Substituído o uso de `use` por `Suspense` para gerenciar o carregamento do componente `OrderDetail`, melhorando a experiência do usuário durante a recuperação dos dados do pedido.
    - Adicionada mensagem de fallback "Carregando..." enquanto os dados estão sendo carregados.
    - Implementada lógica para exibir mensagem de erro caso o pedido não seja encontrado.

--- src/app/(account)/account/orders/[orderId]/page.tsx
+++ src/app/(account)/account/orders/[orderId]/page.tsx
@@ -1,17 +1,22 @@
 import { OrderHttp } from '@/http/Order'
 import OrderDetail from '@/components/OrderDetail'
-import { use } from 'react'
+import { Suspense } from 'react'
 
 export default async function OrderDetailPage({
   params,
 }: {
   params: Promise<{ orderId: string }>
 }) {
-  const { orderId } = use(params)
-
-  const ordersRes = await OrderHttp.getOrderById(Number(orderId))
+  const resolvedParams = await params
+  const ordersRes = await OrderHttp.getOrderById(Number(resolvedParams.orderId))
 
   if (!ordersRes.hasError && ordersRes.data) {
-    return <OrderDetail order={ordersRes.data} />
+    return (
+      <Suspense fallback={<div>Carregando...</div>}>
+        <OrderDetail order={ordersRes.data} />
+      </Suspense>
+    )
   }
+
+  return <div>Pedido não encontrado</div>
 }
